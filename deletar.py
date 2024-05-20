de29e946c5b87e74213600b7da091c6925854d2a
GuiCalmeida87 <guicoutinhoalmeida@gmail.com>
2024-05-20 15:24:52 -0300

    Versão inicial deletar prato

--- deletar.py
+++ deletar.py
@@ -1,13 +1,13 @@
-def remover_prato():
-    deletar=input("escreva o prato que você quer adicionar: ")
-    with open('pratos_adicionados.csv','r',encoding='utf8')as file:
-        linhas=file.readlines()
-    with open('pratos_adiconados.csv','a',encoding='utf8') as file:
-        for linha in linhas:
-            linha.split(':','|')
-            if deletar==linha[1]:
-                linha=""
-                print("prato deletado")
-            else:
-                print("prato não existente")    
-remover_prato()                           
+def deletar_prato(pratos_por_pais, file_name):
+    pais = input("Digite o nome do país: ").title()
+    prato = input("Digite o nome do prato que deseja excluir: ").title()
+
+    if pais in pratos_por_pais and prato in pratos_por_pais[pais]:
+        pratos_por_pais[pais].remove(prato)
+        print("Prato excluído!")
+
+        with open(file_name, 'w') as file:
+            for pais, pratos in pratos_por_pais.items():
+                file.write(f"{pais}: {';'.join(pratos)}\n")
+    else:
+        print("Prato não encontrado.")
